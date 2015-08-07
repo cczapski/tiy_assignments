0771811cf07fd362dab720ebd3a6fd9d81c5affd
cczapski <casieczapski@gmail.com>
2015-08-07 15:15:07 -0500

    updated header with volunteer login and centering of logo

--- final_project/code/js/main.js
+++ final_project/code/js/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
-  $('#cssmenu > ul > li:has(ul)').addClass("has-sub");
+ $(window).resize(function(){
+  if ($(window).width() <= 550){  
+    $('#cssmenu > ul > li:has(ul)').addClass("has-sub");
 
   $('#cssmenu > ul > li > a').click(function() {
     var checkElement = $(this).next();
     
     $('#cssmenu li').removeClass('active');
-    $(this).closest('li').addClass('active');	
+    $(this).closest('li').addClass('active'); 
     
     
     if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
@@ -22,8 +24,94 @@ $(document).ready(function(){
     if (checkElement.is('ul')) {
       return false;
     } else {
-      return true;	
-    }		
+      return true;  
+    }   
   });
 
+  } 
+});
+
+
+ [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+]
+
+  
 });
